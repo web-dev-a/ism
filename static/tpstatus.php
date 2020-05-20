ae36939b43ccf110f0dcd6825e8122a052d1405c
web-dev-a <61269281+web-dev-a@users.noreply.github.com>
2020-05-20 07:33:46 +0300

    Create tpstatus.php

---
+++ static/tpstatus.php
@@ -0,0 +1,96 @@
+<!doctype html>
+<html>
+    <head>
+	<meta name="generator" content="Hugo 0.47" />    
+        
+        <title>Links - I share money</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="google" content="notranslate" />
+        <link href="https://fonts.googleapis.com/css?family=Nunito+Sans:400,400i,700,700i" rel="stylesheet">
+        <link rel="stylesheet" href="/assets/css/main.css">
+        
+    </head>
+    <body>
+        
+        <div id="page" class="site palette-blue">
+
+<main id="content" class="site-content">
+<section id="faq" class="block faq-block bg-gray outer">
+  <div class="inner-small">
+    <div class="block-header">
+    
+    <?php
+
+require('simple_html_dom.php');
+
+// Create DOM from URL or file
+$html = file_get_html('https://www.topparrain.com/en/companies/124%2Fskrill-promo-code');
+
+foreach($dom->getElementsByTagName('h6') as $ptag)
+{
+    if($ptag->getAttribute('class')=="text-uppercase text-muted pt-2")
+    {
+        echo $ptag->nodeValue; //"prints" Some text
+    }
+}
+/?>
+
+
+    
+    
+      <p><img src="https://www.datocms-assets.com/25624/1589863174-ism-transparent.png"/></p>
+      <h2 class="block-title">@easymoneyoffers</h2>
+	    <p></p>
+	      
+	    <p class="block-buttons">
+		    <a href="/how-to-start-earning-money-online/" class="button primary"  style="display: block; width: 100%;">★ Beginner's Guide  </a>
+	    </p>
+	    
+	    <p class="block-buttons">
+		    <a href="/transfer/azimo/" class="button secondary" style="display: block; width: 100%;">Azimo 10€</a>
+	    </p>
+	    
+	    <p class="block-buttons">
+		    <a href="/cryptocurrency/bitpanda/" class="button secondary" style="display: block; width: 100%;">Bitpanda 15€</a>
+	    </p>
+	    
+	    <p class="block-buttons">
+		    <a href="/bank/bitwala/" class="button secondary" style="display: block; width: 100%;">Bitwala 30€</a>
+	    </p>
+	    
+	    <p class="block-buttons">
+		    <a href="/transfer/skrill/" class="button secondary" style="display: block; width: 100%;">Skrill 15€</a>
+	    </p>
+	    
+	    <p class="block-buttons">
+		    <a href="/transfer/transfergo/" class="button secondary" style="display: block; width: 100%;">TransferGo 17€</a>
+	    </p>
+	    
+	    <p class="block-buttons">
+		    <a href="/cryptocurrency/xapo/" class="button secondary" style="display: block; width: 100%;">Xapo 10€</a>
+	    </p>
+	    
+	    <p class="block-buttons">
+		    <a href="/transfer/xendpay/" class="button secondary" style="display: block; width: 100%;">XendPay 10€</a>
+	    </p>
+	    
+	    <p class="block-buttons">
+		    <a href="/transfer/worldremit/" class="button secondary" style="display: block; width: 100%;">WorldRemit 25€</a>
+	    </p>
+	    
+	    <p class="block-buttons">
+		    <a href="/offers" class="button primary" style="display: block; width: 100%;">All offers</a>
+	    </p>
+
+	    <p class="block-buttons">
+		    <a href="mailto:how@ishare.money" class="button primary" style="display: block; width: 100%;">✉ Need help?</a>
+	    </p>
+    </div>
+  </div>
+</section>
+          </main>
+        </div>
+        
+    <script src="https://widget.stackbit.com/init.js" id="stackbit-widget-init" data-stackbit-project-id="5e9092a363f2b70019f6c2fb"></script></body> 
+</html>
